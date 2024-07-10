0b72c59ed019dddd23cc01e2067682123387572f
Esraa Abduallah <esraahisham753@gmail.com>
2024-07-10 17:00:35 +0000

    Working on Python variables and operators

---
+++ welcome.py
@@ -0,0 +1,10 @@
+line01 = '********************'
+line02 = "*     Welcome      *"
+line03 = "*                  *"
+
+print('')
+print(line01)
+print(line03)
+print(line02)
+print(line03)
+print(line01)
