7abee21b0e693a57bbbcae20eb1ab4f0b12a0fc0
Esraa Abduallah <esraahisham753@gmail.com>
2024-07-11 17:00:27 +0000

    String manipulation

---
+++ data_types.py
@@ -0,0 +1,16 @@
+first = "Esraa"
+second = "Abduallah"
+
+fullname = first + " " + second
+print(fullname)
+fullname += "!"
+print(fullname)
+
+multiline = '''
+Hey!
+      just checking in
+               All good?
+'''
+
+print(multiline)
+print(len(multiline))
